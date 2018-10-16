9ae6336cf96a3f20984053a79603b50ddca5856d
soyu1234 <smileprew@gmail.com>
2018-10-16 10:54:17 +0300

    Updates

---
+++ precache-manifest.e676047bc6f3c14b6262aa253f30081e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/dumb-robots/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "a2e4046c4d3a53af4bce",
+    "url": "/dumb-robots/static/js/runtime~main.a2e4046c.js"
+  },
+  {
+    "revision": "b2dbc1ebc23c9e552dc4",
+    "url": "/dumb-robots/static/js/main.b2dbc1eb.chunk.js"
+  },
+  {
+    "revision": "0ea688f289915e1413cb",
+    "url": "/dumb-robots/static/js/1.0ea688f2.chunk.js"
+  },
+  {
+    "revision": "b2dbc1ebc23c9e552dc4",
+    "url": "/dumb-robots/static/css/main.0a330b90.chunk.css"
+  },
+  {
+    "revision": "0ea688f289915e1413cb",
+    "url": "/dumb-robots/static/css/1.cdc0a65e.chunk.css"
+  },
+  {
+    "revision": "4f80c7408f82a12732a6e3419fec6a6e",
+    "url": "/dumb-robots/index.html"
+  }
+];
