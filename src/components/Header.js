b5d8355a3858bce3754981ba3c8e734ec835c061
soyu1234 <smileprew@gmail.com>
2018-10-16 10:51:59 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return false;
+  }
+  render() {
+    return <h1 className="f1">Dumb Robots</h1>;
+  }
+}
+export default Header;
